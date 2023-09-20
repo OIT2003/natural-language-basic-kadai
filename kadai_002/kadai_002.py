c712501407a5db55d0c32e7a11534ce817d470a6
OIT2003 <118606664+OIT2003@users.noreply.github.com>
2023-09-20 12:43:43 +0900

    Add files via upload

--- kadai_002/kadai_002.py
+++ kadai_002/kadai_002.py
@@ -1,21 +1,34 @@
 import re
+import MeCab
 from bs4 import BeautifulSoup
 from urllib import request
 
-url = "https://www.aozora.gr.jp/cards/000148/files/2371_13943.html"
+main_url = "https://www.aozora.gr.jp/cards/000148/files/2371_13943.html"
+stop_url = "http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt"
 
-response = request.urlopen(url)
-soup = BeautifulSoup(response)
+main_response = request.urlopen(main_url)
+main_soup = BeautifulSoup(main_response)
 
-response.close()
+stop_response = request.urlopen(stop_url)
+stop_soup = BeautifulSoup(stop_response)
 
-main_text = soup.find("div", class_ = "main_text")
-tags_to_delete = main_text.find_all(["rp", "rt"])
+main_response.close()
+stop_response.close()
 
-for tag in tags_to_delete:
+main_text = main_soup.find("div", class_ = "main_text")
+main_tags_delete = main_text.find_all(["rp", "rt"])
+
+for tag in main_tags_delete:
     tag.decompose()
 
 main_text = main_text.get_text()
 main_text = re.sub(r"[\u3000 \n \r]", "", main_text)
+
+stop_text = stop_soup.text
+stoptext_list = stop_text.split("\r\n")
+stoptext_list = [word for word in stoptext_list if word]
+
+mecab_tagger = MeCab.Tagger()
+main_text = mecab_tagger.parse(main_text)
 
 print(main_text)
